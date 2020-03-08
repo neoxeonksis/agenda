6f4ee0673add427d20eb20755285406006782619
Carlos Sanchez <carlos.david.sanchez.moreno@hotmail.com>
2020-03-08 14:34:47 -0700

    set up

---
+++ config/settings/local.py
@@ -0,0 +1,12 @@
+from config.settings.base import *
+
+STATIC_ROOT = BASE_DIR + "/static/"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+DEBUG = True
